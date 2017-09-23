24af8a9ffe393b2e47fe9de0edc33ac47312d794
tototo0710 <toto@katou-yuki-no-MacBook-Pro.local>
2017-09-23 16:39:35 +0900

    edit files

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,39 +1,31 @@
 require 'rails_helper'
 describe Message do
-  describe '#message-success01' do
-    it "メッセージがあれば保存できる" do
+  context '#message-success' do
+    example "メッセージがあれば保存できる" do
       message = build(:message, image: nil)
       expect(message).to be_valid
     end
-  end
-  describe '#message-success02' do
-    it "画像があれば保存できる" do
+    example "画像があれば保存できる" do
       message = build(:message, body: nil)
       expect(message).to be_valid
     end
-  end
-  describe '#message-success03' do
-    it "メッセージと画像があれば保存できる" do
+    example "メッセージと画像があれば保存できる" do
       message = build(:message)
       expect(message).to be_valid
     end
   end
-  describe '#message-error01' do
-    it "メッセージも画像も無いと保存できない" do
+  context '#message-error' do
+    example "メッセージも画像も無いと保存できない" do
       message = build(:message, body: nil, image: nil)
       message.valid?
       expect(message.errors[:body_or_image]).to include("を入力してください")
     end
-  end
-  describe '#message-error02' do
-    it "group_idが無いと保存できない" do
+    example "group_idが無いと保存できない" do
       message = build(:message, group_id: nil)
       message.valid?
       expect(message.errors[:group]).to include("を入力してください")
     end
-  end
-  describe '#message-error03' do
-    it "user_idが無いと保存できない" do
+    example "user_idが無いと保存できない" do
       message = build(:message, user_id: nil)
       message.valid?
       expect(message.errors[:user]).to include("を入力してください")
